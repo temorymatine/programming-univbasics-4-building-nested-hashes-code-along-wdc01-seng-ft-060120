7fcd7be95f635dd9589a01e65996f2506bdcb5b0
Matine temory <temorymatine@gmail.com>
2020-05-19 19:07:45 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
         :name => "Lord Capulet", :age => "50"
         },
       :matriarch => {
-        :name => "Lady Capulet", :age => , "51"
+        :name => "Lady Capulet", :age =>  "51"
         },
       :heroine => {
         :name =>  "Juliet", :age =>  "15", :status => "alive"
