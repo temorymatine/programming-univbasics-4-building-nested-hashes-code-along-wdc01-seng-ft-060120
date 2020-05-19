5f23f2776a9a4a7af5603d85d1e9838ee4836a18
Matine temory <temorymatine@gmail.com>
2020-05-19 19:00:23 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague" , :age => "53"
         },
       :matriarch => {
-      :name => {} , :age => {}
+      :name => "Lady Montague", :age =>  "54"
         },
       :hero => {
         :name => {} , :age => {}, :status {}
