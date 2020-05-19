83f53baeb951f87678efafc5a8f40e698330b59c
Matine temory <temorymatine@gmail.com>
2020-05-19 18:59:23 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => {} , :age => {}
+        :name => "Lord Montague" , :age => "53"
         },
       :matriarch => {
       :name => {} , :age => {}
